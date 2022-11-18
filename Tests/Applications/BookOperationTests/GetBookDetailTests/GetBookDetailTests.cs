497f7a1f5ea5bc5560d3ebc7bd4a3b1e9466a2e9
yamurzuko <yamurzcan@gmail.com>
2022-11-18 07:42:17 +0300

    GetBook Detail Query Tests

---
+++ Tests/Applications/BookOperationTests/GetBookDetailTests/GetBookDetailTests.cs
@@ -0,0 +1,60 @@
+﻿using AutoMapper;
+using BookStore.Application.BookOperations.GetBookDetail;
+using BookStore.DBOperations;
+using BookStore.Entities;
+using FluentAssertions;
+using Tests.TestSetup;
+
+namespace Tests.Applications.BookOperationTests.GetBookDetailTests
+{
+    public class GetBookDetailTests : IClassFixture<CommonTestFixture>
+    {
+        private readonly BookStoreDBContext _context;
+
+        private readonly IMapper _mapper;
+
+        public GetBookDetailTests(CommonTestFixture testFixture)
+		{
+            _context = testFixture.Context;
+            _mapper = testFixture.Mapper;
+		}
+
+        [Fact]
+
+        public void WhenBookIdIsGivenNotInDataBase_InvalidOperationException_ShouldBeReturn()
+        {
+            GetBookDetailQuery command = new GetBookDetailQuery(_context, _mapper);
+
+            FluentActions
+                .Invoking(() => command.Handle())
+                .Should().Throw<InvalidOperationException>().And.Message.Should().Be("Kitap Bulunamadı");
+        }
+
+        [Fact]
+
+        public void WhenValidBookIdIsGiven_Book_ShouldBeGetting()
+        {
+            var book = new Book()
+            {
+                Title = "Kırk Haramiler",
+                GenreId = 1,
+                AuthorId = 1,
+                PageCount = 456,
+                PublishDate = new System.DateTime(2000, 09, 21)
+            };
+
+            _context.Books.Add(book);
+            _context.SaveChanges();
+
+            GetBookDetailQuery command = new GetBookDetailQuery(_context, _mapper);
+            command.BookId = book.Id;
+
+            FluentActions.Invoking(() => command.Handle()).Invoke();
+
+            book = _context.Books.SingleOrDefault(x => x.Id == command.BookId);
+
+            book.Should().NotBeNull();
+        }
+    }
+}
+
---
+++ Tests/Applications/BookOperationTests/GetBookDetailTests/GetBookDetailValidatorTests.cs
@@ -0,0 +1,41 @@
+﻿using BookStore.Application.BookOperations.GetBookDetail;
+using FluentAssertions;
+using Tests.TestSetup;
+
+namespace Tests.Applications.BookOperationTests.GetBookDetailTests
+{
+    public class GetBookDetailValidatorTests : IClassFixture<CommonTestFixture>
+    {
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+
+        public void WhenInvalidBookIdIsGiven_Validator_ShouldBeReturnErrors(int bookId)
+        {
+            GetBookDetailQuery command = new GetBookDetailQuery(null, null);
+            command.BookId = bookId;
+
+            GetBookDetailQueryValidator validator = new GetBookDetailQueryValidator();
+            var result = validator.Validate(command);
+
+            result.Errors.Count.Should().BeGreaterThan(0);
+        }
+
+        [Theory]
+        [InlineData(10)]
+        [InlineData(1)]
+
+        public void WhenInvalidBookIdAreGiven_Validator_ShouldNotBeReturnErrors(int bookId)
+        {
+            GetBookDetailQuery command = new GetBookDetailQuery(null, null);
+            command.BookId = bookId;
+
+            GetBookDetailQueryValidator validator = new GetBookDetailQueryValidator();
+            var result = validator.Validate(command);
+
+            result.Errors.Count.Should().Be(0);
+        }
+    }
+}
+
