d06482c2a82a63d58fdb28df457eba3dd9fbce9d
yamurzuko <yamurzcan@gmail.com>
2022-11-19 02:59:59 +0300

    CreateAuthor Tests

---
+++ Tests/Applications/AuthorOperationTests/CreateAuthorTests/CreateAuthorCommandTests.cs
@@ -0,0 +1,73 @@
+﻿using AutoMapper;
+using BookStore.Application.AuthorOperations.CreateAuthor;
+using BookStore.DBOperations;
+using BookStore.Entities;
+using FluentAssertions;
+using Tests.TestSetup;
+
+namespace Tests.Applications.AuthorOperationTests.CreateAuthorTests
+{
+    public class CreateAuthorCommandTests : IClassFixture<CommonTestFixture>
+	{
+		private readonly BookStoreDBContext _context;
+
+		private readonly IMapper _mapper;
+
+		public CreateAuthorCommandTests(CommonTestFixture testFixture)
+		{
+			_context = testFixture.Context;
+			_mapper = testFixture.Mapper;
+		}
+
+		[Fact]
+
+		public void WhenAlreadyExistAuthorNameIsGiven_InvalidOperationException_ShouldBeReturn()
+		{
+			var author = new Author()
+			{
+				Name = "WhenAlreadyExistAuthorNameIsGiven_InvalidOperationException_ShouldBeReturn",
+				Surname = "Yağmur",
+				BirthDate = new System.DateTime(1997, 05, 24)
+			};
+
+			_context.Authors.Add(author);
+			_context.SaveChanges();
+
+			CreateAuthorCommand command = new CreateAuthorCommand(_context, _mapper);
+			command.Model = new CreateAuthorModel()
+			{
+				Name = author.Name
+			};
+
+			FluentActions
+				.Invoking(() => command.Handle())
+				.Should().Throw<InvalidOperationException>().And.Message.Should().Be("Yazar zaten mevcut");
+		}
+
+		[Fact]
+
+        public void WhenValidInputsAreGiven_Author_ShouldBeCreated()
+        {
+            CreateAuthorCommand command = new CreateAuthorCommand(_context, _mapper);
+            CreateAuthorModel model = new CreateAuthorModel()
+            {
+                Name = "Uğur Can",
+                Surname = "Yağmur",
+                BirthDate = new System.DateTime(1990, 04, 04)
+            };
+
+            command.Model = model;
+
+            FluentActions
+                .Invoking(() => command.Handle()).Invoke();
+
+            var author = _context.Authors.SingleOrDefault(x => x.Name == model.Name);
+
+            author.Should().NotBeNull();
+            author.Surname.Should().Be(model.Surname);
+            author.BirthDate.Should().Be(model.BirthDate);
+
+        }
+    }
+}
+
---
+++ Tests/Applications/AuthorOperationTests/CreateAuthorTests/CreateAuthorCommandValidatorTests.cs
@@ -0,0 +1,68 @@
+﻿using BookStore.Application.AuthorOperations.CreateAuthor;
+using FluentAssertions;
+using Tests.TestSetup;
+
+namespace Tests.Applications.AuthorOperationTests.CreateAuthorTests
+{
+    public class CreateAuthorCommandValidatorTests : IClassFixture<CommonTestFixture>
+	{
+		[Theory]
+		[InlineData(" ", " ")]
+        [InlineData("ug", "yag")]
+        [InlineData("ugurca", "yag")]
+        [InlineData("ugu", "yagmur")]
+
+        public void WhenInvalidInputAreGiven_Validator_ShouldBeReturnErrors(string name, string surname)
+		{
+			CreateAuthorCommand command = new CreateAuthorCommand(null, null);
+			command.Model = new CreateAuthorModel()
+			{
+				Name = name,
+				Surname = surname,
+                BirthDate = DateTime.Now.Date.AddYears(-1),
+            };
+
+			CreateAuthorCommandValidator validator = new CreateAuthorCommandValidator();
+			var result = validator.Validate(command);
+
+			result.Errors.Count.Should().BeGreaterThan(0);
+		}
+
+        [Fact]
+
+        public void WhenDateTimeNowIsGiven_Validator_ShouldBeReturnErrors()
+        {
+            CreateAuthorCommand command = new CreateAuthorCommand(null, null);
+            command.Model = new CreateAuthorModel()
+            {
+                Name = "UğurCan",
+                Surname = "Yağmur",
+                BirthDate = DateTime.Now.Date,
+            };
+
+            CreateAuthorCommandValidator validator = new CreateAuthorCommandValidator();
+            var result = validator.Validate(command);
+
+            result.Errors.Count.Should().BeGreaterThan(0);
+        }
+
+        [Fact]
+
+        public void WhenValidInputsAreGiven_Validator_ShouldNotBeReturnErrors()
+        {
+            CreateAuthorCommand command = new CreateAuthorCommand(null, null);
+            command.Model = new CreateAuthorModel()
+            {
+                Name = "Uğur Can",
+                Surname = "Yağmur",
+                BirthDate = DateTime.Now.Date.AddYears(-2)
+            };
+
+            CreateAuthorCommandValidator validator = new CreateAuthorCommandValidator();
+            var result = validator.Validate(command);
+
+            result.Errors.Count.Should().Be(0);
+        }
+    }
+}
+
