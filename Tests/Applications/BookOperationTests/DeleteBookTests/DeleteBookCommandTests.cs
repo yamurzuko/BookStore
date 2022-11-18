b3a645070081dc9dea92375588080d63cac8b098
yamurzuko <yamurzcan@gmail.com>
2022-11-18 07:11:18 +0300

    Delete Book Tests

---
+++ Tests/Applications/BookOperationTests/DeleteBookTests/DeleteBookCommandTests.cs
@@ -0,0 +1,57 @@
+﻿using BookStore.Application.BookOperations.DeleteBook;
+using BookStore.DBOperations;
+using BookStore.Entities;
+using FluentAssertions;
+using Tests.TestSetup;
+
+namespace Tests.Applications.BookOperationTests.DeleteBookTests
+{
+    public class DeleteBookCommandTests : IClassFixture<CommonTestFixture>
+	{
+		private readonly BookStoreDBContext _context;
+
+		public DeleteBookCommandTests(CommonTestFixture testFixture)
+		{
+			_context = testFixture.Context;
+		}
+
+		[Fact]
+
+		public void WhenAlreadyExistBookIdIsGiven_InvalidOperationException_ShouldBeReturn()
+		{
+			DeleteBookCommand command = new DeleteBookCommand(_context);
+
+			FluentActions
+				.Invoking(() => command.Handle())
+				.Should().Throw<InvalidOperationException>().And.Message.Should().Be("Kitap Bulunamadı");
+        }
+
+		[Fact]
+
+		public void WhenValidBookIdAreGiven_Book_ShouldBeDeleted()
+		{
+			var book = new Book()
+			{
+				Title = "Duner",
+				GenreId = 2,
+				AuthorId = 2,
+				PageCount = 1002,
+				PublishDate = new System.DateTime(1999, 06, 07)
+			};
+
+			_context.Books.Add(book);
+			_context.SaveChanges();
+
+			DeleteBookCommand command = new DeleteBookCommand(_context);
+
+			command.BookId = book.Id;
+
+			FluentActions.Invoking(() => command.Handle()).Invoke();
+
+			book = _context.Books.SingleOrDefault(x => x.Id == command.BookId);
+
+            book.Should().BeNull();
+        }
+    }
+}
+
---
+++ Tests/Applications/BookOperationTests/DeleteBookTests/DeleteBookCommandValidatorTests.cs
@@ -0,0 +1,41 @@
+﻿using BookStore.Application.BookOperations.DeleteBook;
+using FluentAssertions;
+using Tests.TestSetup;
+
+namespace Tests.Applications.BookOperationTests.DeleteBookTests
+{
+    public class DeleteBookCommandValidatorTests : IClassFixture<CommonTestFixture>
+	{
+
+		[Theory]
+		[InlineData(0)]
+		[InlineData(-1)]
+
+		public void WhenInvalidBookIdAreGiven_Validator_ShouldBeReturnErrors(int bookId)
+		{
+			DeleteBookCommand command = new DeleteBookCommand(null);
+			command.BookId = bookId;
+
+			DeleteBookCommandValidator validator = new DeleteBookCommandValidator();
+			var result = validator.Validate(command);
+
+            result.Errors.Count.Should().BeGreaterThan(0);
+        }
+
+		[Theory]
+		[InlineData(10)]
+        [InlineData(1)]
+
+        public void WhenInvalidBookIdAreGiven_Validator_ShouldNotBeReturnErrors(int bookId)
+		{
+            DeleteBookCommand command = new DeleteBookCommand(null);
+            command.BookId = bookId;
+
+            DeleteBookCommandValidator validator = new DeleteBookCommandValidator();
+            var result = validator.Validate(command);
+
+            result.Errors.Count.Should().Be(0);
+        }
+    }
+}
+
